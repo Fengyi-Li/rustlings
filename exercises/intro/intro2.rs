a1b4e083aeff85af622930d752acc52fcd109da3
Fengyi Li <68875335+Fengyi-Li@users.noreply.github.com>
2023-09-07 22:46:08 +0000

    Pending changes exported from your codespace

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    println!("Hello {}!");
+    let mut owned_string: String = "hello ".to_owned();
+    let borrowed_string: &str = "world";
+
+    owned_string.push_str(borrowed_string);
+    println!("Hello {}!", owned_string);
 }
